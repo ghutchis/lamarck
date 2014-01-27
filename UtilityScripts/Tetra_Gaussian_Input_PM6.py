09e81be6458a1ffb0f930c7435f5eec2bb828a0a
Ilana Kanal <iek2@pitt.edu>
2014-01-27 10:03:48 -0500

    Created utility script folder with mutate script, tetramer input script and copied Utils.py from SolarGA

---
+++ UtilityScripts/Tetra_Gaussian_Input_PM6.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+
+# File takes the input file, which is specified on the command line and creates tetramers for each monomer
+# and creates a gaussian input file (.com) for each tetramer.
+
+import sys
+import pybel
+import openbabel
+
+def globalopt(mol, debug=False, fast=False):
+    pybel._builder.Build(mol.OBMol)
+    mol.addh()
+
+    ff = pybel._forcefields["mmff94"]
+    success = ff.Setup(mol.OBMol)
+    if not success:
+        ff = pybel._forcefields["uff"]
+        success = ff.Setup(mol.OBMol)
+        if not success:
+            sys.exit("Cannot set up forcefield")
+
+    ff.SteepestDescent(1000, 1.0e-4)    
+    ff.WeightedRotorSearch(250, 25)
+    ff.WeightedRotorSearch(250, 25)
+    ff.ConjugateGradients(500, 1.0e-6)
+    ff.GetCoordinates(mol.OBMol)
+
+header = "%%nproc=4\n#T PM6 OPT(MaxCycle=1000)"
+header_b = "\n\n"
+
+datafile = open(sys.argv[1])
+
+for line in datafile:
+    line = line.rstrip()
+    name = line.split()[1] + '-' + line.split()[2]
+    name = name.replace(':', '-')
+    tetramer = line.split()[0] * 4
+    mol = pybel.readstring("smi", tetramer)
+    print name
+
+    globalopt(mol)
+
+    gaussian = (header + "\n\n" + name + "\n" + "\n".join(mol.write("gau").replace("0  3\n", "0  1\n").split("\n")[3:]))
+
+    output = open("%s.com" % name, "w")
+    output.write(gaussian)
+    output.close()
