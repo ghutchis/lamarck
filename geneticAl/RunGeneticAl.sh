77c043c710f223d1811d0e26cf0782466fb2279e
Ilana Kanal <iek2@pitt.edu>
2014-03-18 10:12:30 -0400

    Script to submit genetic algorithm

--- geneticAl/RunGeneticAl.sh
+++ geneticAl/RunGeneticAl.sh
@@ -4,4 +4,4 @@
 #Change the output file name before each run. If the output file name already exists, the text output file won't be saved.
 #Seed, length, number in the population (N), matrix nbrs and objective can also be changed as needed
 
-python  geneticAl.py -d output/Run3_9_1 --seed=1 --length=6 -N 6 --matrix=mostsim4.json --nbrs=7 --objective=distance
+python  geneticAl.py -d output/Run3_18_1 --seed=1 --length=6 -N 6 --matrix=mostsim4.json --nbrs=7 --objective=distance
