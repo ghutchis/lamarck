092cad9e042d1bf673d83dc9d475b2c97bee6b8a
Ilana Kanal <iek2@pitt.edu>
2014-09-03 09:55:33 -0400

    Makes 2 lists of most common monomers from GeneticAl output files

--- geneticAl/Analysis_Phase2/MonomerCounts.py
+++ geneticAl/Analysis_Phase2/MonomerCounts.py
@@ -4,25 +4,41 @@ from collections import Counter
 
 
 # Manually convert .db file to text file.
-dataFile = sys.argv[1]
-dataSet = open(dataFile)
 
-all_monomers = []
+dataFile1 = sys.argv[1]
+dataSet1 = open(dataFile1)
 
-for line in dataSet.readlines():
+dataFile2 = sys.argv[2]
+dataSet2 = open(dataFile2)
+
+all_monomers_1 = []
+all_monomers_2 = []
+
+for line in dataSet1.readlines():
     # pull the two smiles
     smiles = line.split("\"")[1].split("_")[0].split("~")
-    all_monomers.append(smiles[0])
-    all_monomers.append(smiles[1])
+    all_monomers_1.append(smiles[0])
+    all_monomers_1.append(smiles[1])
 
-all_monomers.sort()
+for line in dataSet2.readlines():
+    # pull the two smiles
+    smiles = line.split("\"")[1].split("_")[0].split("~")
+    all_monomers_2.append(smiles[0])
+    all_monomers_2.append(smiles[1])
 
-unique_monos = Counter(all_monomers)
-unique_monos.most_common()
+all_monomers_1.sort()
+all_monomers_2.sort()
 
-file = open(sys.argv[2], "w")
+unique_monos_1 = Counter(all_monomers_1)
+unique_monos_1.most_common()
 
-for key, count in unique_monos.iteritems():
-    file.write("%s\t%i\n" % (key, count))
+unique_monos_2 = Counter(all_monomers_1)
+unique_monos_2.most_common()
 
-file.close()
+
+#file = open(sys.argv[3], "w")
+
+#for key, count in unique_monos.iteritems():
+#    file.write("%s\t%i\n" % (key, count))
+
+#file.close()
