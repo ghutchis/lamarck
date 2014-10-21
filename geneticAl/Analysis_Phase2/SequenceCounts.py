b6a733201bbc431d467d06fbce27109efef43ab6
Ilana Kanal <iek2@pitt.edu>
2014-10-21 09:24:23 -0400

    Takes arguments to define output files (json,csv)

--- geneticAl/Analysis_Phase2/SequenceCounts.py
+++ geneticAl/Analysis_Phase2/SequenceCounts.py
@@ -31,10 +31,10 @@ for line in dataSet1.readlines():
                 monomers[mon] = {sequence: 1}
 
 # save the dictdict (see http://stackoverflow.com/a/7100202/168775)
-with open('SequenceCounts.json', 'wb') as fp:
+with open(sys.argv[2], 'wb') as fp:
     json.dump(monomers, fp)
 
 # replace the NaNs with 0 (see http://stackoverflow.com/a/13295801/168775)
 monomerDF = pd.DataFrame(monomers).transpose().fillna(0)
-
-monomerDF.to_csv('SequenceCounts.csv')
+monomerDF.to_csv(sys.argv[3])
+#monomerDF.to_csv('SequenceCounts.csv')
